cab2c1c900c3017a79b1efa13aafefccf143400a
Asadali0707 <109643118+Asadali0707@users.noreply.github.com>
2023-04-26 22:54:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+const scrollLeft = document.querySelector(".scroll-left");
+const scrollRight = document.querySelector(".scroll-right");
+const heroDiv = document.querySelector(".hero-img");
+const sectionContainer = document.querySelector("section");
+const bodyContainer = document.querySelector("body");
+const emblemDiv = document.querySelector(".emblem");
+const albumTitleSpan = document.querySelector(".album-title");
+const texts = document.querySelectorAll(".text");
+const albumNum = document.querySelector(".album-num");
+const spotifyWidget = document.querySelector(".spotify-widget iframe");
+const albums = [
+	<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/6TpwDliETwXYHfWXFVaXZr?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+];
+
+scrollLeft.addEventListener("click", () => handleClickScroll(-1));
+scrollRight.addEventListener("click", () => handleClickScroll(1));
+heroDiv.addEventListener("animationend", () => {
+	heroDiv.classList.remove("album-transition");
+	document.addEventListener("keydown", handleKeyScroll);
+	scrollLeft.disabled = false;
+	scrollRight.disabled = false;
+	scrollLeft.classList.remove("key-press-hover-left");
+	scrollRight.classList.remove("key-press-hover-right");
+
+	for (const text of texts) text.classList.add("show-texts");
+});
+
+const handleClickScroll = (val) => {
+	if (index + val >= 0 && index + val < albums.length) {
+		updateDisplay((index += val));
+	}
+};
+
+const handleKeyScroll = (e) => {
+	if (e.key == "ArrowLeft") {
+		scrollLeft.classList.add("key-press-hover-left");
+		handleClickScroll(-1);
+	}
+	if (e.key == "ArrowRight") {
+		scrollRight.classList.add("key-press-hover-right");
+		handleClickScroll(1);
+	}
+};
+let index = 0;
+
+const updateDisplay = (index) => {
+	let DELIMITER = "";
+
+	const album = albums[index];
+
+	for (const text of texts) text.classList.remove("show-texts");
+	emblemDiv.innerHTML = "";
+	scrollLeft.disabled = true;
+	scrollRight.disabled = true;
+	document.removeEventListener("keydown", handleKeyScroll);
+
+	sectionContainer.id = `hero-${album.album.toLowerCase().replace(" ", "-")}`;
+	bodyContainer.style.background = `linear-gradient(180deg, ${album["bg-color"][0]} 0%, ${album["bg-color"][1]} 100%)`;
+	heroDiv.style.backgroundImage = `url(${album.url})`;
+	albumTitleSpan.textContent = album.album;
+	spotifyWidget.src = album.spotify;
+
+	const number = index + 1;
+	albumNum.innerText = number >= 10 ? number + "." : `0${number}.`;
+	albumNum.style.color = album["accent-color"];
+
+	if (index === 3) scrollRight.classList.add("hide-arrow");
+	else scrollRight.classList.remove("hide-arrow");
+
+	createEmblem(album.emblem, DELIMITER[0] || undefined).forEach((node) =>
+		emblemDiv.append(node)
+	);
+
+	heroDiv.classList.add("album-transition");
+};
+
+const createEmblem = (string, delimiter = "•") => {
+	const spans = [];
+
+	string = string.trim().replaceAll(" ", delimiter) + delimiter;
+	const numChars = string.length;
+	const degVal = 90 / (numChars / 4);
+
+	string.split("").forEach((char, idx) => {
+		const span = document.createElement("span");
+		span.innerText = char;
+		span.style.transform = `rotate(${180 - degVal * idx}deg)`;
+		if (char === delimiter) span.style.color = albums[index]["accent-color"];
+		spans.push(span);
+	});
+
+	return spans;
+};
+
+updateDisplay(index);
